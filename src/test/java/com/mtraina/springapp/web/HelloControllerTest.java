5521306bb206a0377d2518cb5a420086d67922d4
mtraina <matteo.traina.mail@gmail.com>
2016-06-17 11:56:12 +0200

    assertj and controller test added

---
+++ src/test/java/com/mtraina/springapp/web/HelloControllerTest.java
@@ -0,0 +1,16 @@
+package com.mtraina.springapp.web;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HelloControllerTest {
+	
+	@Test
+	public void handleRequestView() throws Exception {
+		HelloController controller = new HelloController();
+		ModelAndView modelAndView = controller.handleRequest(null, null);
+		assertThat(modelAndView.getViewName()).isEqualTo("WEB-INF/jsp/hello.jsp");
+	}
+}
